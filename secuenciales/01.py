4bec71b7d00b38c272bf80c11767423d0b6762da
jhomarlino47470166 <jhomarlino@gmail.com>
2025-06-29 17:18:43 -0500

    001.py

--- secuenciales/01.py
+++ secuenciales/01.py
@@ -1,1 +1,13 @@
-print("hola mundo")
+import os
+os.system("cls")
+
+Varones = float(input("Ingrese la cantidad de varones:"))
+Mujeres = float(input("Ingrese la cantidad de mujeres:"))
+
+Total = Varones + Mujeres
+
+pVarones = Varones / Total * 100
+pMujeres = Mujeres / Total * 100
+
+print(f"el porcentaje de varones es: {pVarones:.2f} %")
+print(f"el porcentaje de mujeres es: {pMujeres:.2f} %")
