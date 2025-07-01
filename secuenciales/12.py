7de0a895088ea637d124c0d57f234b07ce511266
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-01 18:28:59 -0500

    Update.py

--- secuenciales/12.py
+++ secuenciales/12.py
@@ -2,18 +2,16 @@
 # de la forma ax² + bx + c. Usando la fórmula general y la clase math.
 
 import os
-import math
 os.system("cls")
 
 a = float(input("Ingrese el valor de a: "))
 b = float(input("Ingrese el valor de b: "))
 c = float(input("Ingrese el valor de c: "))
 
-discriminante = b**2 - 4 * a * c
+d = b**2 - 4 * a * c
 
-if discriminante < 0:
-    print("La ecuación no tiene soluciones reales.")
-else:
-    x1 = (-b + math.sqrt(discriminante)) / (2 * a)
-    x2 = (-b - math.sqrt(discriminante)) / (2 * a)
-    print(f"Las soluciones son: x1 = {x1:.2f}, x2 = {x2:.2f}")
+x1 = (-b + d**0.5) / (2*a)
+x2 = (-b - d**0.5) / (2*a)
+
+print(f"x1 = {x1:.2f}")
+print(f"x2 = {x2:.2f}")
