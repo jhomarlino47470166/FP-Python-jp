bb167e59d8a9a8aba5928b6f03e5b9604197c38f
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:05:32 -0500

    Update 03.py

--- secuenciales/03.py
+++ secuenciales/03.py
@@ -1,4 +1,3 @@
-#Una persona ha recorrido tres tramos de una carretera. La longitud del primer tramo está dada en kilómetros, el segundo tramo en pies y el tercer tramo en millas. Desarrolle el programa que determine la longitud total recorrida en metros y en yardas. Considere los siguientes factores de conversión: 1 metro = 3.2808 pies, 1 milla = 1609 metros. 
 
 import os
 os.system ("cls")
