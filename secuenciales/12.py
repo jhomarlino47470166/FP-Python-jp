150efb8866ebec1f9facb893031d6b2b825411c2
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:10:36 -0500

    Update 12.py

--- secuenciales/12.py
+++ secuenciales/12.py
@@ -1,17 +1,19 @@
-# Desarrolle el programa que permita calcular el resultado de la ecuación de segundo grado 
-# de la forma ax² + bx + c. Usando la fórmula general y la clase math.
 
 import os
+import math
+
 os.system("cls")
 
 a = float(input("Ingrese el valor de a: "))
 b = float(input("Ingrese el valor de b: "))
 c = float(input("Ingrese el valor de c: "))
 
-d = b**2 - 4 * a * c
+discriminante = b**2 - 4 * a * c
 
-x1 = (-b + d**0.5) / (2*a)
-x2 = (-b - d**0.5) / (2*a)
+raiz = math.sqrt(discriminante)  
 
-print(f"x1 = {x1:.2f}")
-print(f"x2 = {x2:.2f}")
+x1 = (-b + raiz) / (2 * a)
+x2 = (-b - raiz) / (2 * a)
+
+print("La primera solución es:", x1)
+print("La segunda solución es:", x2)
