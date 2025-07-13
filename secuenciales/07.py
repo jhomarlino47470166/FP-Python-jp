71ff4c08887ff9bffd2d20a4fa13601e151cf46f
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:07:06 -0500

    Update 07.py

--- secuenciales/07.py
+++ secuenciales/07.py
@@ -1,5 +1,3 @@
-#Desarrolle el programa que permite convertir una cantidad dada en metros a su equivalente en centímetros, pulgadas, pies y yardas. Considere los siguientes factores de conversión: 
-#1 metro = 100 cm, 1 pie = 12 pulgadas, 1 yarda = 3 pies, 1 pulgada = 2.54 cm
 
 import os
 os.system ("cls")
