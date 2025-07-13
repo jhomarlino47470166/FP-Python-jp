e6c9521b7237e28ec2ec63f9a5d27c13365c9cd9
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:09:02 -0500

    Update 09.py

--- secuenciales/09.py
+++ secuenciales/09.py
@@ -1,16 +1,14 @@
-# Desarrolle el programa que lea un número entero de 4 cifras y determine la suma de sus cifras.
-# Asumir que el número es positivo y tiene exactamente 4 cifras.
 
 import os
 os.system("cls")
 
 numero = int(input("Ingrese un número de 4 cifras: "))
 
-d1 = numero // 1000
-d2 = (numero // 100) % 10
-d3 = (numero // 10) % 10
-d4 = numero % 10
+millar = numero // 1000
+centena = (numero // 100) % 10
+decena = (numero // 10) % 10
+unidad = numero % 10
 
-suma = d1 + d2 + d3 + d4
+suma_cifras = millar + centena + decena + unidad
 
-print(f"La suma de las cifras es: {suma}")
+print(f"\nLa suma de las cifras es: {suma_cifras}")
