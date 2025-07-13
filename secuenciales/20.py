93ad663121c181422ad5996a46f52118173f174b
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:13:27 -0500

    Update 20.py

--- secuenciales/20.py
+++ secuenciales/20.py
@@ -1,40 +1,37 @@
-# Dado un monto en soles, descomponer en billetes de 200, 100, 50, 20, 10
-# y monedas de 5, 2 y 1 sol.
-
 import os
 os.system("cls")
 
 monto = int(input("Ingrese la cantidad de dinero en soles: "))
 
-b200 = monto // 200
-monto %= 200
+billetes200 = monto // 200
+monto = monto % 200  # Lo que sobra
 
-b100 = monto // 100
-monto %= 100
+billetes100 = monto // 100
+monto = monto % 100
 
-b50 = monto // 50
-monto %= 50
+billetes50 = monto // 50
+monto = monto % 50
 
-b20 = monto // 20
-monto %= 20
+billetes20 = monto // 20
+monto = monto % 20
 
-b10 = monto // 10
-monto %= 10
+billetes10 = monto // 10
+monto = monto % 10
 
-m5 = monto // 5
-monto %= 5
+monedas5 = monto // 5
+monto = monto % 5
 
-m2 = monto // 2
-monto %= 2
+monedas2 = monto // 2
+monto = monto % 2
 
-m1 = monto
+monedas1 = monto  # Lo que queda son monedas de 1 sol
 
-print("Descomposición:")
-print(f"Billetes de 200: {b200}")
-print(f"Billetes de 100: {b100}")
-print(f"Billetes de 50: {b50}")
-print(f"Billetes de 20: {b20}")
-print(f"Billetes de 10: {b10}")
-print(f"Monedas de 5: {m5}")
-print(f"Monedas de 2: {m2}")
-print(f"Monedas de 1: {m1}")
+print("\nDescomposición del monto en billetes y monedas:")
+print(f"Billetes de 200: {billetes200}")
+print(f"Billetes de 100: {billetes100}")
+print(f"Billetes de 50: {billetes50}")
+print(f"Billetes de 20: {billetes20}")
+print(f"Billetes de 10: {billetes10}")
+print(f"Monedas de 5: {monedas5}")
+print(f"Monedas de 2: {monedas2}")
+print(f"Monedas de 1: {monedas1}")
