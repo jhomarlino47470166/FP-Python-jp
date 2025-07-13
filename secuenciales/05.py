0f52028def0700879ff143f6c527033f24cec584
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-13 16:06:30 -0500

    Update 05.py

--- secuenciales/05.py
+++ secuenciales/05.py
@@ -1,4 +1,3 @@
-# Desarrolle el programa que, dada la capacidad de un disco duro en gigabytes, lo convierta a megabytes, kilobytes y bytes. 1 KB = 1024 bytes, 1 MB = 1024 KB, 1 GB = 1024 MB.
 
 import os
 os.system("cls")
