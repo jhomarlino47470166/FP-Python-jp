ef72a9f4c56e5afa01390467d582b4b079844dec
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-29 19:33:18 -0500

    Update 22.py

--- condicionales/22.py
+++ condicionales/22.py
@@ -1,22 +1,41 @@
 import os
 os.system("cls")
 
-cantA = int(input("Ingrese cantidad de producto A: "))
-cantB = int(input("Ingrese cantidad de producto B: "))
+productoA = int(input("Ingresa cantidad de prodcuto A : "))
+productoB = int(input("Ingresa cantidad de prodcuto B : "))
 
-precioA = 25
-precioB = 30
+costoA = 25
+costoB = 30
 
-importeA = cantA * precioA
-importeB = cantB * precioB
+importeA = productoA * costoA
+importeB = productoB * costoB
 
-descA = importeA * 0.15 if cantA > 50 else 0
-descB = importeB * 0.10 if cantB > 60 else 0
+t_importe = importeA + importeB
 
-importe_bruto = importeA + importeB
-descuento = descA + descB
-total = importe_bruto - descuento
+if productoA > 50 :
+    descuentoA = importeA * 0.15
+else :
+    descuentoA = 0
 
-print(f"Importe bruto: S/. {importe_bruto:.2f}")
-print(f"Descuento: S/. {descuento:.2f}")
-print(f"Total a pagar: S/. {total:.2f}")
+if productoB > 60 :
+    descuentoB = importeB * 0.10
+else : 
+    descuentoB = 0
+
+t_descuento = descuentoA + descuentoB
+
+t_pagar = t_importe - t_descuento
+
+print("DETALLE DE COMPRA")
+print(f"Importe de producto A : S/ {importeA}")
+print(f"Importe de producto B : S/ {importeB}")
+print(f"Importe total : S/ {t_importe}")
+print(f"Descuento aplicado : S/ {t_descuento}")
+print(f"Total a pagar : S/ {t_pagar}")
+
+
+
+
+
+
+
